50290f131e99f141d00f69df125147c2f374e172
root <root@java.local>
2022-02-18 16:39:55 -0500

    go

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,20 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type SidesType int
+const  SidesTriangle SidesType = 3
+const  SidesSquare SidesType = 4
+const  SidesCircle SidesType = 0
+
+func CalcSquare(sideLen float64, sidesNum SidesType) float64 {
+	switch sidesNum {
+		case SidesTriangle:
+			return sideLen*sideLen*math.Sqrt(3)/4
+		case SidesSquare:
+			return sideLen*sideLen
+		case SidesCircle:
+			return sideLen*sideLen*math.Pi
+		default:
+			return 0
+	}
 }
